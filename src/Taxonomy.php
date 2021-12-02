05d9bd1aff052509db0164ab8538bd5c8ab20aec
Quentin Gabriele <quentin.gabriele@gmail.com>
2021-12-01 19:34:04 -0500

    setup taxonomy class with basic insert and get

--- src/Taxonomy.php
+++ src/Taxonomy.php
@@ -12,30 +12,68 @@ class Taxonomy extends Base
     public function findOrCreate($name)
     {
         $name = ucfirst(strtolower($name));
-        $term = get_term_by('name', $name, $this->term_name);
-        if ($term) {
-            return $term;
-        } else {
+        $term = static::init()->findByName($name);
+
+        if (!$term) {
             wp_insert_term(
                 $name,
                 $this->term_name
             );
-            $term = get_term_by('name', $name, $this->term_name);
+            $term = static::init()->findByName($name);
         }
 
         return $term;
     }
 
-    public static function find($id)
+    /**
+     * @param int|null $limit
+     * @return array
+     */
+    public function get($limit = null)
+    {
+        $this->initQueryBuilder();
+
+        if ($limit) {
+            $this->limit($limit);
+        }
+
+        $posts = get_terms(
+            $this->queryBuilder->buildWpQuery()
+        );
+
+        $posts = array_map(
+            function ($item) {
+                $data = $item->to_array();
+
+                return new static(
+                    $data,
+                    true
+                );
+            },
+            $posts
+        );
+
+        return $posts;
+    }
+
+    public function find($id)
     {
         return new self(get_term($id)->to_array());
+    }
+
+    public function findByName($name)
+    {
+        $term = get_term_by('name', $name, $this->term_name);
+        if (!$term) return null;
+
+        return new self($term->to_array());
     }
 
     public function children()
     {
         $this->children = array_map(
             function ($item) {
-                return self::find($item);
+                return self::init()->find($item);
             },
             get_term_children($this->term_id, $this->term_name)
         );
@@ -50,5 +88,14 @@ class Taxonomy extends Base
             'slug' => $this->slug,
             'count' => $this->count,
         ];
+    }
+
+    protected function initQueryBuilder()
+    {
+        if (!$this->queryBuilder) {
+            $this->queryBuilder = new QueryBuilder();
+            $this->queryBuilder->where('taxonomy', '=', $this->term_name);
+            $this->queryBuilder->where('hide_empty', '=', false);
+        }
     }
 }
